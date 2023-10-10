6efefca1dac3a0f62f8d46d3508a7f3f67cff70f
leekoby <abc2752@naver.com>
2023-10-11 02:59:49 +0900

    init: 초기 세팅

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '이호승 | 포트폴리오',
+  description: '프론트엔드 개발자 이호승의 포트폴리오 웹사이트입니다.',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='ko'>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+        <div className='bg-[#dbd7fb] absolute top-[-1rem] left-[35rem] h-[31.25rem] w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <div></div>
+        {children}
+      </body>
+    </html>
+  );
+}
