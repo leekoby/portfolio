ce3b7aadfa540f05963beca6d25eda17ca01f5b1
leekoby <abc2752@naver.com>
2023-10-13 23:23:32 +0900

    feat: skill 영역

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import About from '@/components/about';
+import Experience from '@/components/experience';
 import Intro from '@/components/intro';
 import Projects from '@/components/projects';
 import SectionDivider from '@/components/section-divider';
+import Skills from '@/components/skills';
 
 const Home = () => {
   return (
@@ -9,7 +11,9 @@ const Home = () => {
       <Intro />
       <SectionDivider />
       <About />
+      <Skills />
       <Projects />
+      <Experience />
     </main>
   );
 };
