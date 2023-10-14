5b28f7f2d72fd1338be5d3d59ca039435c689973
leekoby <abc2752@naver.com>
2023-10-15 01:46:09 +0900

    feat: 이미지 aws s3로 변경

--- lib/data.ts
+++ lib/data.ts
@@ -3,6 +3,8 @@ import { CgWorkAlt } from 'react-icons/cg';
 import { FaReact } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
 
+const baseUrl = process.env.NEXT_PUBLIC_S3_BASE_URL;
+
 export const links = [
   {
     name: 'Home',
@@ -49,6 +51,7 @@ export const experiencesData = [
     title: 'Frontend bootcamp',
     Affiliation: 'CodeStates',
     description: [
+      '학습 도우미로 선정되어 다른 수강생의 학습을 도움',
       'JavaScript와 React를 중심으로 한 프론트엔드 역량 학습',
       '네트워크, 운영체제, CPU 등의 컴퓨터 공학적 지식 습득',
       '페어 프로그램을 통한 지속적인 과제 수행 및 코드 리뷰 경험',
@@ -72,17 +75,9 @@ export const projectsData = [
       '피그마를 이용한 화면 정의서 구현 / 프로젝트 폴더 구조 및 초기 코드 세팅 / 피드백을 받고 코드를 수정하는 과정을 겪으면서 FE끼리 혹은 FE와 BE 간의 대한 협업에 대한 전반적인 과정에 대해 경험할 수 있었습니다.',
     ],
     tags: ['Next.js', 'TypeScript', 'React-Query', 'Tailwind', 'Zustand'],
-    images: [
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/6e3839a8-1544-4c28-9210-10dad423d58c',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/f1307b87-01ef-4adb-ad63-fc16aca57a6f',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/aa69cf6e-8fa2-45ab-99ac-09f6d9d76a4e',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/1b723c0a-0c0e-4352-b754-2668ceeeeb52',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/cdd1f0fd-c44b-412a-b287-0cf3a97fc1a8',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/7e00030a-d328-45c7-8881-9227d4a878e2',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/60379f6a-b81b-473b-8aec-7022af2dc449',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/6a1ba5c4-7aff-4f7b-b105-9f2710fa8703',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/1c873a37-c4f5-4d64-8e05-11f6d526a6ec',
-    ],
+    images: Array(9)
+      .fill(null)
+      .map((_, i) => `${baseUrl}/cc/cc0${i + 1}.png`),
     deployLink: 'https://www.hard-coding.com/',
     gitHubRepo: 'https://github.com/codestates-seb/seb43_main_013/tree/fe-dev',
   },
@@ -91,14 +86,15 @@ export const projectsData = [
     period: '2023.07. ~ ',
     role: '팀 프로젝트 FE 팀원 역할 ( FE 4, BE 3 ) | 기여도 : 25 %',
     description: [
+      '포스트 코로나 시대의 지역 축제 활성화에 도움이 되고자 시작한 프로젝트입니다.',
       '다양한 축제 정보를 효과적으로 제공하여 사용자가 쉽고 빠르게 흥미로운 축제를 찾아 새로운 경험을 할 수 있도록 도와줍니다.',
+      'FE 팀원으로 회원가입, 로그인, 리뷰댓글 CRUD를 구현했습니다.',
+      '로컬 회원가입/로그인과 OAuth의 로직 전반에 대해 학습할 수 있었습니다.',
     ],
     tags: ['React', 'TypeScript', 'React-Query', 'Styled-Components', 'Zustand'],
-    images: [
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/e2c658f5-21be-4398-bf29-7f5b5e765ec8',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/83ba2d05-7bd4-4f11-9310-3f907124f577',
-      'https://github.com/leekoby/leekoby.github.io/assets/118284808/68d9e2a2-86b5-4e1d-9d63-de72523a522a',
-    ],
+    images: Array(3)
+      .fill(null)
+      .map((_, i) => `${baseUrl}/ticat/ticat0${i + 1}.jpeg`),
     deployLink: 'https://d99pqcn6hzkdg.cloudfront.net/',
     gitHubRepo: 'https://github.com/Butlers-Team/Ticat-client',
   },
